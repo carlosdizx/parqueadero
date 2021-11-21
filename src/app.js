ce918412f2acf8b2971439a277a94e2e9675fdfb
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 13:17:45 -0500

    preparando para configurar la base de datos

--- src/app.js
+++ src/app.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const indexRouter = require("./routes/index.routes");
+const logger = require("morgan");
 
 const app = express();
+
+app.use(express.json());
+app.use(logger("dev"));
 
 app.use(indexRouter);
 
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const indexRouter = express.Router();
 
-indexRouter.get("/", (req, res) => res.send("Hello world!"));
+indexRouter.get("/", (req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json");
+  res.json([]);
+});
 
 module.exports = indexRouter;
