1fa68c56639136054f33112e9c214bdfa3544f2b
Carlos Ernesto Díaz Basante <carlodiaz@umariana.edu.co>
2021-11-21 12:26:31 -0500

    Configurado para funcionar con nodemon el script

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
 
 const app = express();
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+
 app.listen(3000, () => console.log("Servidor enn http://localhost:3000"));
